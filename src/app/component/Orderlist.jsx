68b1555ce30ea97c30f8a184a41493f46adc1b2d
rahul-rlc <rahul.b@richylife.ae>
2024-04-03 19:06:18 +0400

    Gird layout added

--- src/app/component/Orderlist.jsx
+++ src/app/component/Orderlist.jsx
@@ -4,6 +4,10 @@ import styles from '../style/orderedlist.module.css';
 const Orderlist = () => {
   return (
     <div className={styles.maincontainer}>
+      <div className={styles.headingcontainer}>
+        <h2>Order List</h2>
+        <p>View all</p>
+      </div>
       <div className={styles.itemcontainer}>
         <OrderItem />
         <OrderItem />
