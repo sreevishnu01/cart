e4380a8ea780fbfd66809876a83dab0fe5a691af
rahul-rlc <rahul.b@richylife.ae>
2024-04-03 18:47:00 +0400

    Layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,4 @@
-import Image from 'next/image';
-import Card from './component/Card';
-import Dropdown from './component/Dropdown';
-import { category } from './utilss/data';
-import OrderItem from './component/OrderItem';
-import Orderlist from './component/Orderlist';
+import HomeLayout from './component/HomeLayout';
 
 export default function Home() {
   return (
